a00825cd5042cc5ab2e65cdc06959b5871a163bd
chandrainf <surel.chandrapratama@gmail.com>
2021-06-03 05:23:04 +0700

    config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'production',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "SDJF0W850FJ5LDFJ4095809DFJG045FG",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
+    'mongodb+srv://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || 'sysadmin:YBc6bStzgNYMRgss@cluster0.sgrvx.gcp.mongodb.net') +
     '/mernmediastream',
   serverUrl: process.env.serverUrl || 'http://localhost:3000'
 }
