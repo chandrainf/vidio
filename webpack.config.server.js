355d5cd13d8a949f8ad6bbc16a461d7d2596d694
chandrainf <surel.chandrapratama@gmail.com>
2021-06-03 05:59:28 +0700

    webpack config

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -5,7 +5,7 @@ const CURRENT_WORKING_DIR = process.cwd()
 
 const config = {
     name: "server",
-    entry: [ path.join(CURRENT_WORKING_DIR , './server/server.js') ],
+    entry: [ path.join(CURRENT_WORKING_DIR , './server/server.mjs') ],
     target: "node",
     output: {
         path: path.join(CURRENT_WORKING_DIR , '/dist/'),
